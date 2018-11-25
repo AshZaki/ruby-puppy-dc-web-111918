c4abfb472750fd72edd985a73e2a46fbbd93c61b
Ashley  Zaki <ahnahn.un@gmail.com>
2018-11-25 03:19:12 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,18 +7,17 @@ class Dog
   def initialize(name)
     @name = name
     @@all << self
-    binding.pry
+    
   end
   
   def self.all
     @@all.each do |dog|
-      binding.pry
+    
     end
   end
   
   def self.clear_all
     @@all.clear
-    binding.pry
     return @@all
   end
   
